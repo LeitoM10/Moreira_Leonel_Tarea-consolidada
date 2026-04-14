fc2c837fc132bd621dc3f2b9804cb6d04059d5ca
LeitoM10 <leocuypulga10@gmail.com>
2026-04-14 17:40:44 -0500

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+            Vendedor vendedor = new Vendedor(
+                "Andrea López",
+                "Tecnología",
+                2850.75,
+                8,
+                92
+        );
 
+        vendedor.mostrarDatos();
 }
